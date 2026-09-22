b65108af26970ca4f22e0ceae67324b9a29c69cf
Michael Haupt <subscriptions@michaelhaupt.com>
2026-09-22 18:53:01 +0200

    Quartz sync: Sep 22, 2026, 6:53 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,18 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <div id="quartz-body">
+      {children}
+      <div class="gtranslate_wrapper"></div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.gtranslateSettings = {"default_language":"en","detect_browser_language":true,"languages":["en","af"],"wrapper_selector":".gtranslate_wrapper"};`,
+        }}
+      />
+      <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
+    </div>
+  )
 }
 
 export default (() => Body) satisfies QuartzComponentConstructor
